c995c8ba041da7c2b10d61b269c46cbedea5939c
shyandsy <shyandsy@gmail.com>
2019-01-31 05:19:00 -0600

    update document and format code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,30 @@
-## 0.0.1
+v0.0.1
 
-* TODO: Support AES 128 bit CBC with Padding 7 encrytion and descryption: Cipher2.encryptAesCbc128Padding7 and Cipher2.decryptAesCbc128Padding7(encryptedString, key, iv);
+- Support AES 128 bit CBC with Padding 7 encrytion and descryption for android system.
+- method Cipher2.encryptAesCbc128Padding7(plaintext, key, iv);
+- method Cipher2.decryptAesCbc128Padding7(encryptedString, key, iv);
+
+v0.1.0
+
+- AES CBC PKCSPADDING7 for both ios and android
+
+v0.2.0
+
+- Add exception handle for the invalid parameter
+
+v0.2.1
+
+- [bugfix], Add return statement after result.error in kotlin code to avoid the error message in console.
+
+v0.3.0
+
+- Support AES 128 bit GCM encrytion and descryption (for android now)
+- method Cipher2.encryptAesGcm128(plaintext, key, iv);
+- method Cipher2.decryptAesGcm128(encryptedString, key, iv);
+
+v0.3.1
+
+- update the CHANGELOG.md
+- update the README.md
+- update the widget test for example project
+- format the lib/cipher2.dart
--- lib/cipher2.dart
+++ lib/cipher2.dart
@@ -2,22 +2,18 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Cipher2 {
-  static const MethodChannel _channel =
-      const MethodChannel('cipher2');
+  static const MethodChannel _channel = const MethodChannel('cipher2');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+  static Future<String> encryptAesCbc128Padding7(
+          String data, String key, String iv) async =>
+      await _channel.invokeMethod("Encrypt_AesCbc128Padding7", {
+        "data": data,
+        "key": key,
+        "iv": iv,
+      });
 
-  static Future<String> encryptAesCbc128Padding7(String data, String key, String iv) async =>
-    await _channel.invokeMethod("Encrypt_AesCbc128Padding7", {
-      "data": data,
-      "key": key,
-      "iv": iv,
-    });
-
-  static Future<String> decryptAesCbc128Padding7(String data, String key, String iv) async {
+  static Future<String> decryptAesCbc128Padding7(
+      String data, String key, String iv) async {
     final decrypted = await _channel.invokeMethod("Decrypt_AesCbc128Padding7", {
       "data": data,
       "key": key,
@@ -27,16 +23,18 @@ class Cipher2 {
   }
 
   static Future<String> generateNonce() async =>
-    await _channel.invokeMethod("Generate_Nonce", {});
+      await _channel.invokeMethod("Generate_Nonce", {});
 
-  static Future<String> encryptAesGcm128(String data, String key, String nonce) async =>
-    await _channel.invokeMethod("Encrypt_AesGcm128", {
-      "data": data,
-      "key": key,
-      "nonce": nonce,
-    });
+  static Future<String> encryptAesGcm128(
+          String data, String key, String nonce) async =>
+      await _channel.invokeMethod("Encrypt_AesGcm128", {
+        "data": data,
+        "key": key,
+        "nonce": nonce,
+      });
 
-  static Future<String> decryptAesGcm128(String data, String key, String nonce) async {
+  static Future<String> decryptAesGcm128(
+      String data, String key, String nonce) async {
     final decrypted = await _channel.invokeMethod("Decrypt_AesGcm128", {
       "data": data,
       "key": key,
